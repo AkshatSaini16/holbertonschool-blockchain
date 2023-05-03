3daa30613fbb200e5bfa617006a39a13b91f3cfa
maroua199525 <2646@holbertonstudents.com>
2023-05-03 10:28:30 +0100

    add changes

--- blockchain/v0.1/block_hash.c
+++ blockchain/v0.1/block_hash.c
@@ -1,18 +1,19 @@
 #include "blockchain.h"
 
 /**
- * block_hash - hashes a block
- * @block: pointer to the block to be hashed
- * @hash_buf: pointer to the buffer where the hash will be stored
- * Return: pointer to the hash_buf
- **/
+ * block_hash - computes hash of block
+ * @block: pointer to block to hash
+ * @hash_buf: buffer to store hash/digest
+ * Return: pointer to buffer
+ */
 uint8_t *block_hash(block_t const *block,
-			uint8_t hash_buf[SHA256_DIGEST_LENGTH])
+					uint8_t hash_buf[SHA256_DIGEST_LENGTH])
 {
 	size_t len;
 
 	if (!block || !hash_buf)
 		return (NULL);
+
 	len = sizeof(block->info) + block->data.len;
 	sha256((int8_t const *)&(block->info), len, hash_buf);
 	return (hash_buf);
