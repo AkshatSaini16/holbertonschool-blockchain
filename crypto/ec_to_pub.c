95b03ec17b3e08911c1153bee25f18d8d0928638
maroua199525 <2646@holbertonstudents.com>
2023-07-11 09:18:38 +0100

    add changes

--- crypto/ec_to_pub.c
+++ crypto/ec_to_pub.c
@@ -10,7 +10,6 @@ uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN])
 {
 	const EC_GROUP *group;
 	const EC_POINT *point;
-	size_t len;
 
 	if (!key || !pub)
 		return (NULL);
@@ -19,7 +18,7 @@ uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN])
 	point = EC_KEY_get0_public_key(key);
 	if (!group || !point)
 		return (NULL);
-	len = EC_POINT_point2oct(group, point,
+	EC_POINT_point2oct(group, point,
 			POINT_CONVERSION_UNCOMPRESSED, pub, EC_PUB_LEN, NULL);
 	return (pub);
 }
