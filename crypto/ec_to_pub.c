8cdb6d3a89c53d05952d857fb7462a23758966e3
maroua199525 <2646@holbertonstudents.com>
2023-07-11 09:09:53 +0100

    add changes

--- crypto/ec_to_pub.c
+++ crypto/ec_to_pub.c
@@ -1,23 +1,27 @@
 #include "hblk_crypto.h"
 
 /**
- * ec_to_pub - a function that converts an EC key to a public key.
- * @key: s a pointer to the EC_KEY structure to retrieve
- * the public key from. If it is NULL, your function must do nothing and fail
- * @pub:is the address at which to store
- * the extracted public key (The key shall not be compressed)
- * Return: pointer to public key, NULL on error
- **/
+ * ec_to_pub - extracts the public key from an EC_KEY opaque structure
+ * @key: a pointer to the EC_KEY structure
+ * @pub: the address at which to store the extracted public key
+ * Return: a pointer to pub, NULL on failure,
+ */
 uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN])
 {
+	const EC_GROUP *group;
 	const EC_POINT *point;
+	size_t len;
 
-	if (key == NULL)
+	if (!key || !pub)
 		return (NULL);
 
+	group = EC_KEY_get0_group(key);
 	point = EC_KEY_get0_public_key(key);
-	EC_POINT_point2oct(EC_KEY_get0_group(key), point,
+	if (!group || !point)
+		return (NULL);
+	len = EC_POINT_point2oct(group, point,
 			POINT_CONVERSION_UNCOMPRESSED, pub, EC_PUB_LEN, NULL);
-
+	if (len != EC_PUB_LEN)
+		return (NULL);
 	return (pub);
 }
