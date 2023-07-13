61bfe5939b9205f88065731b93de89db2d80f3f5
maroua199525 <2646@holbertonstudents.com>
2023-07-13 13:34:54 +0100

    add test

--- crypto/ec_save.c
+++ crypto/ec_save.c
@@ -1,32 +1,28 @@
 #include "hblk_crypto.h"
 
 /**
- * ec_save - a function that saves an EC key to a file.
- * @key: pointer to EC key air to be saved on disk
- * @folder: is the path to the folder in which to save the keys
- * folder should be created if it does not exist.
- * Return: return 1 or 0 upon success or failure
+ * ec_save - Save an EC key pair on the disk
+ * @key: Pointer to the EC key pair to be saved
+ * @folder: Path to the folder in which to save the keys
+ *
+ * Return: 1 on success, 0 on failure
  */
 int ec_save(EC_KEY *key, char const *folder)
 {
 	FILE *fp;
-	char path[256] = {0};
+	char path[256];
 
 	if (!key || !folder)
 		return (0);
-
-	mkdir(folder, 0700);
-	sprintf(path, "%s/%s", folder, PRI_FILENAME);
+	mkdir(folder, 0777);
+	sprintf(path, "%s/key.pem", folder);
 	fp = fopen(path, "w");
 	if (!fp)
 		return (0);
-	if (!PEM_write_ECPrivateKey(fp, key, NULL, NULL, 0, NULL, NULL))
-	{
-		fclose(fp);
-		return (0);
-	}
+	PEM_write_ECPrivateKey(fp, key, NULL, NULL, 0, NULL, NULL);
 	fclose(fp);
-	sprintf(path, "%s/%s", folder, PUB_FILENAME);
+
+	sprintf(path, "%s/key_pub.pem", folder);
 	fp = fopen(path, "w");
 	if (!fp)
 		return (0);
@@ -36,5 +32,7 @@ int ec_save(EC_KEY *key, char const *folder)
 		return (0);
 	}
 	fclose(fp);
+
 	return (1);
+
 }
