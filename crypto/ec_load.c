4396f12fc53affd3178477e7fade04425ce12592
maroua199525 <2646@holbertonstudents.com>
2023-07-14 08:57:51 +0100

    add file

--- crypto/ec_load.c
+++ crypto/ec_load.c
@@ -16,25 +16,26 @@ EC_KEY *ec_load(char const *folder)
 	if (!folder)
 		return (NULL);
 
-	sprintf(key_path, "%s/%s", folder, PUB_FILENAME);
+	sprintf(key_path, "%s/%s", folder, PRI_FILENAME);
 
 	fp = fopen(key_path, "r");
 	if (!fp)
 		return (NULL);
 
-	key = PEM_read_EC_PUBKEY(fp, &key, NULL, NULL);
+	key = PEM_read_ECPrivateKey(fp, &key, NULL, NULL);
 	if (!key)
 		return (NULL);
 	fclose(fp);
 
-	sprintf(key_path, "%s/%s", folder, PRI_FILENAME);
+	sprintf(key_path, "%s/%s", folder, PUB_FILENAME);
 	fp = fopen(key_path, "r");
 	if (!fp)
 	{
 		EC_KEY_free(key);
 		return (NULL);
 	}
-	key = PEM_read_ECPrivateKey(fp, &key, NULL, NULL);
+	key = PEM_read_EC_PUBKEY(fp, &key, NULL, NULL);
+	
 	if (!key)
 	{
 		EC_KEY_free(key);
