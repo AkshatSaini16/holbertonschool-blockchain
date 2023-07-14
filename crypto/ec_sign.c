b83d2d38ffc13e5582f5f4adbc3da340c463a3e9
maroua199525 <2646@holbertonstudents.com>
2023-07-14 10:02:56 +0100

    add task

--- crypto/ec_sign.c
+++ crypto/ec_sign.c
@@ -1,24 +1,26 @@
 #include "hblk_crypto.h"
 
 /**
- * ec_sign - Sign a set of bytes with an EC private key
- * @key: pointer to the private key
- * @msg: pointer to the message to sign
- * @msglen: length of the message to sign
- * @sig: pointer to the signature structure
- * Return: Pointer to the signature buffer, or NULL on error.
+ * ec_sign - Sign a given set of bytes using a given EC_KEY private key
+ *
+ * @key:     Pointer to the EC_KEY structure containing the private key
+ * @msg:     Pointer to the bytes to be signed
+ * @msglen:  Length of the message
+ * @sig:     Pointer to the sig_t structure to store the signature
+ *
+ * Return:   Pointer to the signature buffer upon success, or NULL upon failure
  */
 uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg,
-				size_t msglen, sig_t *sig)
+		size_t msglen, sig_t *sig)
 {
-	unsigned int len;
+	uint32_t siglen = 0;
 
-	if (key == NULL || msg == NULL || sig == NULL)
+	if (!key || !msg || !sig)
 		return (NULL);
-	len = sig->len;
-	if (ECDSA_sign(0, msg, msglen, sig->sig, &len, (EC_KEY *)key) != 1)
-		return (NULL);
-	sig->len = len;
 
+	bzero(sig->sig, sizeof(sig->sig));
+	if (ECDSA_sign(0, msg, msglen, sig->sig, &siglen, (EC_KEY *)key) != 1)
+		return (NULL);
+	sig->len = siglen;
 	return (sig->sig);
 }
--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -16,6 +16,9 @@
 #define EC_PUB_LEN 65
 #define EC_CURVE NID_secp256k1
 
+/* Maximum signature octet string length (using 256-bit curve) */
+# define SIG_MAX_LEN    72
+
 #define PRI_FILENAME   "key.pem"
 #define PUB_FILENAME   "key_pub.pem"
 
